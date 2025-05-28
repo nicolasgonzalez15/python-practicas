046f14f531809939872b6e4032890a2e4468d3a2
Nicolás <nicolas.gonzalez15@gmail.com>
2025-05-27 22:07:48 -0300

    Modificación de reglas en caso de fuera de rango o caracteres invalidos. No suma intentos y solo incrementa intentos si es menor o mayor al nro secreto.

--- adivinar-numero-v1.py
+++ adivinar-numero-v1.py
@@ -1,36 +1,38 @@
 import random
 
 nroSecreto = random.randint(1,20)
-print(f"Numero secreto: {nroSecreto}")
+
 incorrecto = True
 intentos = 1
 
 while incorrecto and intentos<=5:
+    
     valorIngresado = input("Ingrsar número entre 1 y 20: ")
     
     #Valido si se ingresan números o caracteres
     if(valorIngresado.isdigit()):
+        #Convierto valor a entero
         nroIngresado = int(valorIngresado)
-    else:
-        print("Error. Debe ingresar números enteros entre 1 y 20. Vuelva a intentar nuevamante.")
-        break
 
-    #Valido que sea entre 1 y 20
-    if not(1<=nroIngresado<=20):
-        print("Error. Debe ingresar números enteros entre 1 y 20. Vuelva a intentar nuevamante.")
-        break
-
-    #Valido si es igual, menor o mayor
-    if(nroIngresado == nroSecreto):
-        incorrecto = False
-        #mensaje = "El número ingresado es correcto!"
-    elif  (nroIngresado<nroSecreto):
-        print("El número ingresado es menor al número secreto")
+        #Valido que esté en el rango solicitado
+        if(1<=nroIngresado<=20):
+               #Valido si es igual, menor o mayor
+            if(nroIngresado == nroSecreto):
+                incorrecto = False
+        
+        #Incremento número de intentos SOLO si es número en el rango
+            elif  (nroIngresado<nroSecreto):
+                print("El número ingresado es menor al número secreto")
+                intentos+=1
+            else:
+                print("El número ingresado es mayor al número secreto")
+                intentos+=1
+        #Fuera de rango  
+        else:
+            print("Fuera de rango. Debe ingresar números enteros entre 1 y 20. Vuelva a intentar nuevamante.")
+    #Valor no numérico
     else:
-        print("El número ingresado es mayor al número secreto")
-    
-    #Incremento número de intentos
-    intentos+=1
+        print("Caracteres no válidos. Debe ingresar números enteros entre 1 y 20. Vuelva a intentar nuevamante.")
 
 if(incorrecto==False):
     print(f"Felicitaciones! Usted adivinó el número secreto!")
